0723b6bc62c9d5e57081a363ded1b6f45e198a78
Melissa Coronado <melissa_coronado@hotmail.com>
2023-09-17 20:02:48 -0300

    DesafioEntregable01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule,
